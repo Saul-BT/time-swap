3d33350499c81ceadab0c9d536201f51a15a7865
Saul Blanco Tejero <saul.blanco@sngular.com>
2026-09-12 16:30:44 +0200

    chore: tests preview deployement

--- front/src/app/(site)/[lang]/anuncios/[id]/page.tsx
+++ front/src/app/(site)/[lang]/anuncios/[id]/page.tsx
@@ -1,6 +1,4 @@
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
+import { Button, Stack, Typography } from "@mui/material";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import {
