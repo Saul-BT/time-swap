11f59c4666cc0c53a3711b0e5940f30cb2854bd7
Mari Cruz <maricruz.njie@gmail.com>
2026-09-17 13:11:23 +0200

    create public profile

---
+++ front/src/app/(site)/[lang]/profile/[memberID]/page.tsx
@@ -0,0 +1,28 @@
+import {notFound} from "next/navigation";
+import Section from "@/components/layout/Section";
+import SiteFooter from "@/components/layout/SiteFooter";
+import SiteHeader from "@/components/layout/SiteHeader";
+import MemberProfile from "@/components/profile/MemberProfile";
+import {MEMBERS} from "@/data/members";
+
+export default async function MemberProfilePage({
+  params,
+}: PageProps<"/[lang]/perfil/[memberId]">) {
+  const { memberId } = await params;
+  const member = MEMBERS.find((candidate) => candidate.id === memberId);
+
+  if (!member) {
+    notFound();
+  }
+  return (
+    <>
+      <SiteHeader />
+      <main>
+        <Section>
+          <MemberProfile member={member} />
+        </Section>
+      </main>
+      <SiteFooter />
+    </>
+  );
+}
